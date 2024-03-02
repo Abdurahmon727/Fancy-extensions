5e62c0b96927ff623127ec90d9ecf64375455193
Abdurahmon <abdurahmon727.uz@gmail.com>
2024-03-02 21:09:52 +0500

    refactor: exports

--- lib/fancy_extensions.dart
+++ lib/fancy_extensions.dart
@@ -1,15 +1,8 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library;
 
 export 'src/generic.dart';
 export 'src/generic_nullable.dart';
 export 'src/list.dart';
 export 'src/list_nullable.dart';
-export 'src/string_nullable.dart';
 export 'src/string.dart';
-
-
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/string_nullable.dart';
